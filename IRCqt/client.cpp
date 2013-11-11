bc87f3dee42a2c90ea7b20070ee543456d9728fe
raphael <bouterige.r@gmail.com>
2013-11-11 20:01:46 +0100

    sendRep restaurée

--- IRCqt/client.cpp
+++ IRCqt/client.cpp
@@ -94,19 +94,15 @@ void Client::setIdcmd(uint16_t idCmd) {
 void Client::sendRep(uint8_t coderetour, string aenvoyer)
 {
 	aenvoyer+="\n";
-	uint16_t idComSpontane = 65534 ;
+	//uint16_t idComSpontane = 65534 ;
 	uint16_t tailleTrame=aenvoyer.size()+3;
 	if (write(fdClient, &tailleTrame, sizeof(uint16_t)) == -1) {
 		perror("Perror_sendMsg write client");
 	}
-	if(coderetour > 127)
-		if (write(fdClient, &idComSpontane, sizeof(uint16_t)) == -1) {
-			perror("Perror_sendMsg write idcmd");
-		}
-	else
-		if (write(fdClient, &idCmd, sizeof(uint16_t)) == -1) {
+	//if(coderetour > 127)
+	if (write(fdClient, &idCmd, sizeof(uint16_t)) == -1) {
 		perror("Perror_sendMsg write idcmd");
-		}
+	}
 	if (write(fdClient, &coderetour, sizeof(uint8_t)) == -1) {
 		perror("Perror_sendMsg write client");
 	}
